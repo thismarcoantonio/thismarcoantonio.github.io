c64d2a9e798a0acee1de3ca5cf59ece33aebb5ca
Marco Antônio <mjota51@gmail.com>
2017-04-28 01:08:10 -0300

    A few changes

--- content/script.js
+++ content/script.js
@@ -1,1 +1,8 @@
-alert('coming soon!');
+var dots = document.querySelector('.dots');
+
+setInterval(function(){
+	dots.innerHTML += '.';
+	if(dots.innerHTML == '....'){
+		dots.innerHTML = '';
+	}
+}, 1000);
